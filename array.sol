0468f6e02e74e1a5ee2239b534940c3b20e326df
Hiro <laciferin@gmail.com>
2023-09-04 05:52:27 +0530

    constructor:arr

--- array.sol
+++ array.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.0;
    contract ArraysExample {
        uint[] public dynamicArray = [1, 2, 3, 4, 5]; //FIXME expected to set dynamicArray but not working
 
-    
+       constructor(){
+        dynamicArray = [1,2,3]
+       }
 
+    
        function getThirdElement() public view returns (uint) {
            return dynamicArray[2];
        }
