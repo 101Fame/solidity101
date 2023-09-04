a4e2c00b519d8707a6f57863e17d61716a60df82
Hiro <laciferin@gmail.com>
2023-09-04 05:34:19 +0530

    minor: simpleStorage

--- simpleStorage.sol
+++ simpleStorage.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract SimpleStorage {
-    uint256 private favoriteNumber;
+    uint private favoriteNumber;
 
     // Function to set the favorite number
-    function setFavoriteNumber(uint256 newFavoriteNumber) public {
+    function store(uint256 newFavoriteNumber) public {
         favoriteNumber = newFavoriteNumber;
     }
 
     // Function to get the favorite number
-    function getFavoriteNumber() public view returns (uint256) {
+    function get() public view returns (uint256) {
         return favoriteNumber;
     }
 }
